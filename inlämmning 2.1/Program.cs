bdeb7d3f38f81c6c388cffe0ad1a9cb9a5d417c6
alsv06003 <alsv06003@E-PW0234JW>
2023-09-15 13:05:19 +0200

    Add project files.

---
+++ inlämmning 2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vad är din favoritbok");
+            string bok = Console.ReadLine();
+
+            Console.WriteLine("Din favoritbok är " + bok + " det är en riktig bra bok");
+        }
+    }
+}
